51f7bfb775e9dd899a6ba2a94b056ebb48f9a1a8
KINGS=IKE <ogbonnayakingsike@gmail.com>
2023-04-14 14:11:55 +0100

    color toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import { Button, ButtonGroup } from '@chakra-ui/react'
-import './App.css'
+import { Button, ButtonGroup, Grid, GridItem, Show } from '@chakra-ui/react'
+import { NavBar } from './components/NavBar/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <Button colorScheme='green'> Button </Button>
+    <Grid templateAreas={{
+      base: `"nav" "main"`,
+      lg: `"nav nav" "aside main"`
+    }}>
+      <GridItem area='nav'  >
+        <NavBar />
+      </GridItem>
+      <Show above='lg'>
+
+        <GridItem area='aside' bg='gold' >Aside</GridItem>
+      </Show>
+      <GridItem area='main' bg='dodgerblue' >Main</GridItem>
+
+
+    </Grid>
   )
 }
 
---
+++ src/theme.ts
@@ -0,0 +1,10 @@
+import { extendTheme, ThemeConfig } from "@chakra-ui/react"
+
+const config: ThemeConfig = {
+    initialColorMode: 'dark'
+}
+
+const theme = extendTheme({ config })
+// #3448c5
+
+export default theme
