a854ef13c1b91f4bde57930afd7d5925741640b8
KINGS=IKE <ogbonnayakingsike@gmail.com>
2023-04-21 13:36:31 +0100

    SearchinImplement searchingg bar

--- src/components/SearchingBox/SearchInput.tsx
+++ src/components/SearchingBox/SearchInput.tsx
@@ -1,14 +1,23 @@
-import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react'
+import { Input, InputGroup, InputLeftElement, InputRightElement } from '@chakra-ui/react'
+import { useRef } from 'react'
 import { BsSearch } from 'react-icons/bs'
 
+interface Props {
+    onSearch: (searchText: string) => void
+}
 
-export const SearchInput = () => {
+export const SearchInput = ({ onSearch }: Props) => {
+    const ref = useRef<HTMLInputElement>(null)
     return (
-        <InputGroup>
-            <InputLeftElement children={<BsSearch />} />
-            <Input borderRadius={20} placeholder='Search games ...' variant='filled'>
-            </Input>
-        </InputGroup>
-
+        <form onSubmit={(e) => {
+            e.preventDefault();
+            if (ref.current) onSearch(ref.current.value)
+        }}>
+            <InputGroup>
+                <InputRightElement children={<BsSearch />} />
+                <Input ref={ref} borderRadius={20} placeholder='Search games ...' variant='filled'>
+                </Input>
+            </InputGroup>
+        </form>
     )
 }
